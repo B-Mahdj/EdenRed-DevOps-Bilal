c0eddb04653971c527b05c1a02b08d5f64c27ed9
Mahdjoubi Bilal <bilal.mahdjoubi95@gmail.com>
2025-06-28 18:33:27 +0200

    Delete useless variables in tf code

--- infra-terraform/main.tf
+++ infra-terraform/main.tf
@@ -16,10 +16,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.azure_subscription_id
-  tenant_id       = var.azure_tenant_id
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
 }
 
 module "resource_group" {
