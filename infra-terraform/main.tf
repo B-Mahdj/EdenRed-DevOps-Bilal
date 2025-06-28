aee9dc4a0fe628de577cc95cca9d1ac7480ece60
Mahdjoubi Bilal <bilal.mahdjoubi95@gmail.com>
2025-06-28 15:34:04 +0200

    Add variables for connecting to AZ SP to provider

--- infra-terraform/main.tf
+++ infra-terraform/main.tf
@@ -16,6 +16,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = var.azure_subscription_id
+  tenant_id       = var.azure_tenant_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
 }
 
 module "resource_group" {
