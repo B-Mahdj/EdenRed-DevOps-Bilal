b3519391feaee270371957f4818caaa75add002e
Mahdjoubi Bilal <bilal.mahdjoubi95@gmail.com>
2025-06-28 15:38:11 +0200

    Set variables for connecting to Azure SP

--- infra-terraform/variables.tf
+++ infra-terraform/variables.tf
@@ -14,3 +14,26 @@ variable "image_url" {
   type        = string
   default     = "ghcr.io/b-mahdj/dotnet-app:latest"
 }
+
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "azure_client_id" {
+  description = "Azure Client (Application) ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+  sensitive   = true
+}
