91e8634180abad33690018e488c766f42430774a
Mahdjoubi Bilal <60183331+B-Mahdj@users.noreply.github.com>
2025-06-28 22:22:56 +0200

    Add otel azure monitoring to dotnet app (#2)
    * Add Otel Instrumentation in .net code
    
    * Modify TF configuration to deploy app insights with Otel link to dotnet app

--- dotnet-app/Program.cs
+++ dotnet-app/Program.cs
@@ -1,4 +1,10 @@
+// Import the Azure.Monitor.OpenTelemetry.AspNetCore namespace.
+using Azure.Monitor.OpenTelemetry.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add OpenTelemetry and configure it to use Azure Monitor.
+builder.Services.AddOpenTelemetry().UseAzureMonitor();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
